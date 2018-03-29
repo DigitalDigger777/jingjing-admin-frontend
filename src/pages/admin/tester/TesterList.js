fd8a33d45c6c2895c63c18255c5aac24dbe895cc
korman <korman.yuri@gmail.com>
2018-03-29 11:40:11 +0300

    changed device

--- src/pages/admin/tester/TesterList.js
+++ src/pages/admin/tester/TesterList.js
@@ -11,6 +11,7 @@ import {
     TableRow,
     TableRowColumn,
 } from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import SearchBar from 'material-ui-search-bar'
@@ -57,6 +58,36 @@ export default class TesterList extends React.Component {
         axios.post();
     }
 
+
+    changeName(e, id) {
+        console.log(id);
+        axios.post(this.state.baseUrl + 'user/save', {
+            id: id,
+            pin: this.state.item.pin,
+            role: 'ROLE_TESTER'
+        })
+            .then(response => {
+                console.log(response);
+            })
+            .catch(response => {
+
+            });
+    }
+
+    changePin(e, id) {
+        axios.post(this.state.baseUrl + 'user/save', {
+            id: id,
+            name: this.state.item.name,
+            role: 'ROLE_TESTER'
+        })
+            .then(response => {
+                console.log(response);
+            })
+            .catch(response => {
+
+            });
+    }
+
     changeSearch(){
 
     }
@@ -92,8 +123,20 @@ export default class TesterList extends React.Component {
                         { this.state.items.map((item, key) =>
                             <TableRow  key={key} onClick={ id => this.openDetailShopper(item.id) }>
                                 <TableRowColumn>{item.id}</TableRowColumn>
-                                <TableRowColumn>{item.name}</TableRowColumn>
-                                <TableRowColumn>{item.pin}</TableRowColumn>
+                                <TableRowColumn>
+                                    {/*<span id={`textName` + item.id} style={{cursor:'pointer'}} onClick={(id, name) => this.editName(item.id, item.name)}>{item.name}</span>*/}
+                                    <TextField id={`name` + item.id}
+                                           type="text"
+                                           defaultValue={item.name}
+                                           onChange={(e, id) => this.changeName(e, id)} />
+                                </TableRowColumn>
+                                <TableRowColumn>
+                                    {/*<span id={`textPin` + item.id} style={{cursor:'pointer'}} onClick={(id, name) => this.editPin(item.id, item.pin)}>{item.pin}</span>*/}
+                                    <TextField id={`pin` + item.id}
+                                               type="text"
+                                               defaultValue={item.pin}
+                                               onChange={(e, id) => this.changePin(e, id)} />
+                                </TableRowColumn>
                             </TableRow>
                         )}
                     </TableBody>
