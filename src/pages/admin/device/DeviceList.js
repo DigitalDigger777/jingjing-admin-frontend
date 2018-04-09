40960f028b9e4ba5382ef384e2b4a6fddf1664a0
korman <korman.yuri@gmail.com>
2018-04-09 15:15:00 +0300

    fixed bug with device list

--- src/pages/admin/device/DeviceList.js
+++ src/pages/admin/device/DeviceList.js
@@ -20,9 +20,16 @@ import {
     TableRow,
     TableRowColumn,
 } from 'material-ui/Table';
-import RaisedButton from 'material-ui/RaisedButton';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import QRCode from 'qrcode.react';
 import SearchBar from 'material-ui-search-bar'
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 import axios from 'axios';
 import Config from '../../../Config';
@@ -36,8 +43,14 @@ export default class DeviceList extends React.Component {
 
         this.state = {
             items: [],
+            dialog: {
+                open: false
+            },
+            baseFrontUrl: config.baseFrontUrl,
             baseUrl: config.baseUrl
         };
+
+        this.closeDialog = this.closeDialog.bind(this);
     }
 
     componentWillMount() {
@@ -57,11 +70,66 @@ export default class DeviceList extends React.Component {
         window.location = '/admin/device-detail/' + id;
     }
 
+    download(e, id){
+        const canvas = document.querySelector('.HpQrcode' + id + ' > canvas');
+        e.target.href = canvas.toDataURL();
+        e.target.download = 'QR-' + id + '.png';
+    }
+
+    actionMenuChange(e, index, value) {
+        const action = value.split(':');
+        const id = action[0];
+
+        console.log(action[1], id);
+
+        switch (parseInt(action[1]))
+        {
+            case 0:
+                break;
+            case 1:
+                    //detail
+                    window.location = '/admin/device-detail/' + id;
+                break;
+            case 2:
+                    //assign shopper
+                break;
+            case 3:
+                    //reset
+                break;
+            case 4:
+                    //remove
+                break;
+        }
+    }
+
     changeSearch(){
 
     }
 
+    save(){
+
+    }
+
+    closeDialog(){
+        this.setState({open: false});
+    }
+
     render() {
+
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onClick={this.closeDialog}
+            />,
+            <FlatButton
+                label="Ok"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.save}
+            />,
+        ];
+
         return (
             <Core>
                 <Toolbar style={{marginTop: '15px', paddingTop: '15px', paddingBottom: '15px'}}>
@@ -84,7 +152,6 @@ export default class DeviceList extends React.Component {
                         <TableRow>
                             <TableHeaderColumn>ID</TableHeaderColumn>
                             <TableHeaderColumn>QR Code</TableHeaderColumn>
-                            <TableHeaderColumn>Name</TableHeaderColumn>
                             <TableHeaderColumn>Add Time</TableHeaderColumn>
                             <TableHeaderColumn>Total Hour Used</TableHeaderColumn>
                             <TableHeaderColumn>Total Revenue</TableHeaderColumn>
@@ -92,23 +159,68 @@ export default class DeviceList extends React.Component {
                         </TableRow>
                     </TableHeader>
                     <TableBody displayRowCheckbox={false} showRowHover={true}>
-                        { this.state.items.map((item, key) =>
-                            <TableRow  key={key} onClick={ id => this.openDetailShopper(item[0].id) }>
-                                <TableRowColumn>{item[0].id}</TableRowColumn>
-                                <TableRowColumn></TableRowColumn>
-                                <TableRowColumn>{item[0].name}</TableRowColumn>
-                                <TableRowColumn>{item[1]}</TableRowColumn>
-                                <TableRowColumn>0</TableRowColumn>
-                                <TableRowColumn>0</TableRowColumn>
-                                <TableRowColumn>
-                                    <RaisedButton label="Change" primary={true} onClick={ id => this.openDeviceDetail(item[0].id) }/>
-                                    <RaisedButton label="Reset" primary={true} />
-                                    <RaisedButton label="Remove" primary={true} />
-                                </TableRowColumn>
-                            </TableRow>
+                        { this.state.items.map((item, key) => {
+
+                                const qrURL = this.state.baseFrontUrl + 'public/index.php/payment/start/' + item[0].id;
+
+                                return (<TableRow key={key} onClick={ id => this.openDetailShopper(item[0].id) }>
+                                    <TableRowColumn>{item[0].id}</TableRowColumn>
+                                    <TableRowColumn className={`HpQrcode` + item[0].id}>
+                                        <QRCode value={qrURL} size="64"/>
+                                        <br/>
+                                        <a href="#" onClick={(e, id) => this.download(e, item[0].id)}>Download</a>
+
+                                    </TableRowColumn>
+                                    <TableRowColumn>{item[1]}</TableRowColumn>
+                                    <TableRowColumn>{item[0].deviceStatistics[0].total_hours}</TableRowColumn>
+                                    <TableRowColumn>{item[0].deviceStatistics[0].total_revenue}</TableRowColumn>
+                                    <TableRowColumn>
+                                        <DropDownMenu value={this.state.value} onChange={this.actionMenuChange}>
+                                            <MenuItem value={item[0].id + `:` + 0} primaryText="Select Action"/>
+                                            <MenuItem value={item[0].id + `:` + 1} primaryText="Detail"/>
+                                            <MenuItem value={item[0].id + `:` + 2} primaryText="Assign Shopper"/>
+                                            <MenuItem value={item[0].id + `:` + 3} primaryText="Reset"/>
+                                            <MenuItem value={item[0].id + `:` + 4} primaryText="Remove"/>
+                                        </DropDownMenu>
+                                    </TableRowColumn>
+                                </TableRow>);
+                            }
                         )}
                     </TableBody>
                 </Table>
+
+                <Dialog
+                    title="Warning"
+                    actions={actions}
+                    modal={false}
+                    open={this.state.dialog.open}
+                    onRequestClose={this.closeDialog}
+                >
+                    Are you sure you want to remove this Purifier?
+
+                </Dialog>
+
+                <Dialog
+                    title="Warning"
+                    actions={actions}
+                    modal={false}
+                    open={this.state.dialog.open}
+                    onRequestClose={this.closeDialog}
+                >
+                    Are you sure you want to reset this Purifier?
+                </Dialog>
+
+                <Dialog
+                    title="Change Shopper ID"
+                    actions={actions}
+                    modal={false}
+                    open={this.state.dialog.open}
+                    onRequestClose={this.closeDialog}
+                >
+
+                    Please Assign Shopper to This Purifier
+                    <TextField hintText="Input shopper ID" />
+                </Dialog>
                 {/*<Cells style={{paddingBottom: '100px'}}>*/}
                     {/*{this.state.items.map((item, key) => {*/}
                         {/*return (*/}
