96881fb053cc35bf93465906ce71b885f2d63058
korman <korman.yuri@gmail.com>
2018-04-14 21:23:28 +0300

    add statement admin translation

--- src/translations/admin/statement/StatementList.js
+++ src/translations/admin/statement/StatementList.js
@@ -5,10 +5,20 @@ import LocalizedStrings from 'react-localization';
 
 const strings = new LocalizedStrings({
     en: {
-
+        income: 'Income',
+        time: 'Time',
+        shopperName: 'Shopper Name',
+        purifierID: 'Purifier ID',
+        rate: 'Rate',
+        hours: 'Hours'
     },
     zh: {
-
+        income: 'Text1',
+        time: 'Text2',
+        shopperName: 'Text3',
+        purifierID: 'Text4',
+        rate: 'Text5',
+        hours: 'Text6'
     }
 });
 
